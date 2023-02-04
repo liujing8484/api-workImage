984a9648b8bccad7605f29254df5c0ca529f70ae
liujing8484 <13548648231@163.com>
2023-02-05 00:15:55 +0800

    20230205 添加中间件

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from image.routers import reader, power, point
 import uvicorn
 
@@ -42,6 +43,18 @@ async def redoc_html():
         redoc_js_url="/static/redoc/redoc.standalone.js",
     )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://localhost:8080",
+        "http://192.168.31.186:8080"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(reader.app, prefix="/reader", tags=['读取xlsx文件到数据库'])
 app.include_router(power.app, prefix="/power", tags=['计算受力并保存到数据库'])
